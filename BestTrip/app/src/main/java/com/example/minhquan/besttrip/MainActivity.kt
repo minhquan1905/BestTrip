d51caec6ef8fdc295de76a37754ddc60c449dc98
DESKTOP-6LFEV0I\quynh <quynhdx0305@gmail.com>
2018-07-06 11:19:27 +0700

    Login MVP

--- BestTrip/app/src/main/java/com/example/minhquan/besttrip/MainActivity.kt
+++ BestTrip/app/src/main/java/com/example/minhquan/besttrip/MainActivity.kt
@@ -3,11 +3,38 @@ package com.example.minhquan.besttrip
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.google.android.gms.common.api.Api
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.auth.FirebaseAuth
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.AuthResult
+import com.google.android.gms.tasks.Task
+import android.support.annotation.NonNull
+import com.google.android.gms.tasks.OnCompleteListener
+import android.R.attr.password
+import android.support.v4.app.FragmentActivity
+import android.util.Log
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlin.math.absoluteValue
+import android.R.attr.password
+import com.example.minhquan.besttrip.datafirebase.*
+import com.google.firebase.database.ValueEventListener
+import kotlinx.android.synthetic.main.activity_main.*
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DataSnapshot
+
+
+
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
 }
---
+++ BestTrip/app/src/main/java/com/example/minhquan/besttrip/login/view/ViewItf.kt
@@ -0,0 +1,8 @@
+package com.example.minhquan.besttrip.login.view
+
+interface ViewItf {
+    fun Show_Login_Success()
+    fun Show_Login_Fail()
+    fun Show_Sigin_Success()
+    fun Show_Sigin_Fail()
+}
