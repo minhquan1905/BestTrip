9fd97a3dcc5dcca8d937ee1425cd7bbd482cf03d
Le Anh Thien <thien.lah@gmail.com>
2018-07-10 10:09:02 +0700

    Remove transition

--- BestTrip/app/src/main/java/com/example/minhquan/besttrip/uilogin/MainActivity.kt
+++ BestTrip/app/src/main/java/com/example/minhquan/besttrip/uilogin/MainActivity.kt
@@ -23,8 +23,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        startActivity(Intent(this, RouteActivity::class.java))
-
+        //startActivity(Intent(this, RouteActivity::class.java))
 
         setSupportActionBar(toolBar)
         supportActionBar?.title = ""
