40cb6fb223f1f4187dddf9bdb78caae8299f2db0
DESKTOP-6LFEV0I\quynh <quynhdx0305@gmail.com>
2018-07-10 15:21:01 +0700

    FireBase Update 2

---
+++ BestTrip/app/src/main/java/com/example/minhquan/besttrip/getsetdata/view/GetDataViewItf.kt
@@ -0,0 +1,7 @@
+package com.example.minhquan.besttrip.getsetdata.view
+
+import com.example.minhquan.besttrip.datafirebase.Client
+
+interface GetDataViewItf {
+    fun showDataChild(ob : Client)
+}
