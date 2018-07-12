280ca1004fb1a06983501dd18023b03f80fd8520
Le Anh Thien <thien.lah@gmail.com>
2018-07-12 15:06:19 +0700

    Modify polyline

---
+++ BestTrip/app/src/main/java/com/example/minhquan/besttrip/utils/AnimationUtil.kt
@@ -0,0 +1,18 @@
+package com.example.minhquan.besttrip.utils
+
+
+import android.animation.ObjectAnimator
+import android.view.View
+import kotlinx.android.synthetic.main.activity_route.view.*
+
+
+/*fun View.upBox() {
+
+    val
+    val endBoxX =
+    val endBoxY =
+    val objectAnimator = ObjectAnimator.ofFloat(search_box,'y',)
+    objectAnimator.duration = 650
+    objectAnimator.start()
+}*/
+
